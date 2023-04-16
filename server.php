641fb4a178d511f7c46b4b085f9722a8b6bc1d0a
FerrariMarc0 <marco1010@live.it>
2023-04-16 22:05:26 +0200

    todo-login

--- server.php
+++ server.php
@@ -1,29 +1,61 @@
 <?php
-$param= isset($_GET['index']) ? $_GET['index'] : NULL;
-$newTodo= isset($_POST['newTodo']) ? $_POST['newTodo'] : NULL;
-var_dump($newTodo);
-die();
 
-$todo_list= file_get_contents(__DIR__.'/todo.json');
-$todo_list= json_decode($todo_list, true);
+require_once(__DIR__.'/functions.php');
 
-if($newTodo !== NULL){
-    $todo_list[]= [
-        "text" => $newTodo,
-        "done" => true
-    ];
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    if ($action === 'login') {
+        $foundUser = login($_POST['username'], $_POST['password']);
+        if ($foundUser === null) {
+            header('Content-Type: application/json');
+            $result = json_encode(['error' => 'Utente non trovato']);
+            echo $result;
+            return;
+        }
+
+        header('Content-Type: application/json');
+        unset($foundUser['password']);
+        $result = json_encode($foundUser);
+        echo $result;
+        return;
+    }
 }
-/* var_dump($todo_list); */
 
-/* $todo_list[0]['text']= "HTML"; */
+ //1 - recuperare contenuto todo-list.json
+$database = file_get_contents(__DIR__.'/todo-list.json'); //string
+//$database = 'lettura da db';
 
-$todo_list= json_encode($todo_list);
+//2 - convertire la stringa in un array associativo
+$todo_list = json_decode($database, true); //array
 
-file_put_contents(__DIR__.'/todo.json', $todo_list);
-/* if($param !== NULL){
-    $todo_list= $todo_list[$param];
-} */
-/* var_dump($todo_list); */
+$headers = getallheaders();
+$username = $headers['Username'] ?? $headers['username'];
 
+//GESTIONE AGGIUNTA
+if(isset($_POST['add'])){
+    //operazione add
+    $todo_list = addTodo($todo_list, $_POST);
+}
+
+//GESTIONE CANCELLAZIONE
+if(isset($_POST['delete'])){
+    //operazione add
+    $todo_list = deleteTodo($todo_list, $_POST['delete']);
+}
+
+//GESTIONE MODIFICA DATI
+
+if(isset($_POST['edit'])){
+    //operazione add
+    $todo_list = editTodo($todo_list, $_POST);
+}
+
+$filteredTodoList = array_filter($todo_list, function ($todo) use ($username) {
+    return $todo['username'] === $username;
+});
+$todo_list = $filteredTodoList;
+
+// restituire dati json
 header('Content-Type: application/json');
-echo $todo_list;
+$result = json_encode($todo_list);
+echo $result;
